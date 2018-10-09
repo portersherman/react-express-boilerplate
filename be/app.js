ed1c10faade3d1da2e10dccd57dd16641729d2a2
portersherman <w.porter.sherman@gmail.com>
2018-10-08 22:11:09 -0400

    remove unnecessary logic

--- be/app.js
+++ be/app.js
@@ -2,20 +2,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mongoose = require('mongoose');
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').load();
-}
-
-var uri = process.env.DB;
-mongoose.connect(uri);
-mongoose.Promise = global.Promise;
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', function() {
-  console.log("database connected");
-});
 
 var indexRouter = require('./routes/index');
 
